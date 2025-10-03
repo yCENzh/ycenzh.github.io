47d70d1d253eedca58df114f7800052123e74b15
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-10-03 10:25:23 +0800

    回退

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,9 +2,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './index.css'
-import Layout from './Layout.vue'
 
 export default {
-  ...DefaultTheme,
-  Layout
+  ...DefaultTheme
 } satisfies Theme
