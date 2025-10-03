a2e0dc7ea8f44d1dcaa8ae2ebe1a712a3b9cabaa
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-10-03 08:22:57 +0800

    index.ts

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+// .vitepress/theme/index.ts
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import './index.css'
+
+export default {
+  ...DefaultTheme
+} satisfies Theme
